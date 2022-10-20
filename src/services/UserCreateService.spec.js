8d35ee20c26327fdd6c7498d637202b1d36fadcc
stéfany <stelar.dev@gmail.com>
2022-10-20 18:37:13 -0300

    jest config and create user test ok

---
+++ src/services/UserCreateService.spec.js
@@ -0,0 +1,46 @@
+const UserCreateService = require("./UserCreateService");
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
+
+describe("UserCreateService", () => {
+  let userRepository = null;
+  let userCreateService = null;
+
+  beforeEach(() => {
+    userRepository = new UserRepositoryInMemory();
+    userCreateService = new UserCreateService(userRepository);
+  });
+
+  it("user should be created", async () => {
+
+    const user = {
+      name: "User Test",
+      email: "usertest@mail.com",
+      password: "123"
+    }
+
+    const userCreated = await userCreateService.execute(user);
+
+    expect(userCreated).toHaveProperty("id");
+  });
+
+  it("user should not be created with existing email", async () => {
+    const user1 = {
+      name: "User Test 1",
+      email: "usertest@mail.com",
+      password: "123"
+    };
+
+    const user2 = {
+      name: "User Test 2",
+      email: "usertest@mail.com",
+      password: "456"
+    };
+
+    await userCreateService.execute(user1);
+
+    expect(async () => {
+      await userCreateService.execute(user2)
+    }).rejects.toEqual(new AppError("Já existe uma conta registrada com esse email"));
+  });
+});
