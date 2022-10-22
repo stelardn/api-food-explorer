959dc65d6969f2d1595b6a40e6a7582f3fad6d94
stéfany <stelar.dev@gmail.com>
2022-10-22 12:38:54 -0300

    eslint

--- src/database/knex/migrations/20221021010946_02_createIngredients.js
+++ src/database/knex/migrations/20221021010946_02_createIngredients.js
@@ -1,7 +1,7 @@
-exports.up = knex => knex.schema.createTable("ingredients", table => {
-  table.increments("id");
-  table.integer("meal_id").references("id").inTable("meals").onDelete("CASCADE");
-  table.text("name");
+exports.up = knex => knex.schema.createTable('ingredients', table => {
+    table.increments('id');
+    table.integer('meal_id').references('id').inTable('meals').onDelete('CASCADE');
+    table.text('name');
 });
 
-exports.down = knex => knex.schema.dropTable("ingredients");
+exports.down = knex => knex.schema.dropTable('ingredients');
