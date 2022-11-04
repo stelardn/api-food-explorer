5f073c075c47a854a966e0565dea3caedc505b65
stéfany <stelar.dev@gmail.com>
2022-11-03 21:46:30 -0300

    auth ok

--- src/routes/ingredients.routes.js
+++ src/routes/ingredients.routes.js
@@ -1,9 +1,11 @@
 const IngredientsController = require('../controllers/IngredientsController');
 const ingredientsController = new IngredientsController();
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+
 const { Router } = require('express');
 const ingredientsRouter = Router();
 
-ingredientsRouter.get('/', ingredientsController.show);
+ingredientsRouter.get('/', ensureAuthenticated, ingredientsController.show);
 
 module.exports = ingredientsRouter;
