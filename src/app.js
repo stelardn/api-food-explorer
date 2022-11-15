9b9ec742fd779e5f40f7b4c69e92515940a4bf20
Stéfany Larissa <stelar.dev@gmail.com>
2022-11-15 13:00:53 -0300

    public created (for ingredients)

--- src/app.js
+++ src/app.js
@@ -11,13 +11,16 @@ const { json } = require('express');
 app.use(json());
 
 const { UPLOADS_FOLDER } = require('./configs/uploads');
+const { PUBLIC_FOLDER } = require('./configs/public');
 app.use('/files', express.static(UPLOADS_FOLDER));
+app.use('/public', express.static(PUBLIC_FOLDER));
+
 
 const appRoutes = require('./routes');
 app.use(appRoutes);
 
 const AppError = require('./utils/AppError');
-app.use((error, request, response, next) => {
+app.use((error, request, response, _next) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
             status: 'error',
---
+++ src/configs/public.js
@@ -0,0 +1,7 @@
+const { resolve } = require('path');
+
+const PUBLIC_FOLDER = resolve(__dirname, '..', '..', 'public');
+
+module.exports = {
+    PUBLIC_FOLDER
+};
