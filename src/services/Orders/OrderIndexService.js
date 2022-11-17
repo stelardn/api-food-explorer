36fab1a0d27ed6dce8a9fcda42f6e90982b3cfec
stéfany <stelar.dev@gmail.com>
2022-11-16 21:02:32 -0300

    user show updated with order info

--- src/services/Orders/OrderIndexService.js
+++ src/services/Orders/OrderIndexService.js
@@ -1,4 +1,4 @@
-const AppError = require("../../utils/AppError");
+const AppError = require('../../utils/AppError');
 
 class OrderIndexService {
     constructor(orderRepository, orderItemsRepository) {
@@ -30,5 +30,32 @@ class OrderIndexService {
 
 
     }
+
+    async getUserOrders(user_id) {
+        const userOrders = await this.orderRepository.findUserOrders(user_id);
+
+
+        if (userOrders.length > 0) {
+
+            const allItems = await this.orderItemsRepository.findUserOrderItemsAndOrderInfo(user_id);
+
+            const userOrdersWithItems = userOrders.map(order => {
+                const orderItems = allItems.filter(item => item.id == order.id); // item id é o order id do item
+
+                return {
+                    ...order,
+                    orderItems
+                }
+            })
+
+            return userOrdersWithItems;
+        }
+
+        throw new AppError('Não foi possível buscar os dados.');
+
+
+    }
+
+
 }
 module.exports = OrderIndexService;
