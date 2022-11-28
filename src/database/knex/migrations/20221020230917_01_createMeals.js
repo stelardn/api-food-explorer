6843aec78ccb70c027d18991c42b93ac0638d66e
Stéfany Larissa <stelar.dev@gmail.com>
2022-11-28 20:56:46 -0300

    type column created in meal table

--- src/database/knex/migrations/20221020230917_01_createMeals.js
+++ src/database/knex/migrations/20221020230917_01_createMeals.js
@@ -1,12 +1,14 @@
-exports.up = knex => knex.schema.createTable('meals', table => {
-    table.increments('id');
-    table.text('name');
-    table.text('description');
-    table.float('price', 2);
-    table.text('picture');
+exports.up = (knex) =>
+  knex.schema.createTable("meals", (table) => {
+    table.increments("id");
+    table.text("name");
+    table.text("description");
+    table.float("price", 2);
+    table.text("picture");
+    table.text("type");
 
-    table.timestamp('created_at').default(knex.fn.now());
-    table.timestamp('updated_at').default(knex.fn.now());
-});
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+  });
 
-exports.down = knex => knex.schema.dropTable('meals');
+exports.down = (knex) => knex.schema.dropTable("meals");
