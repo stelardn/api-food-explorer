05837919a9ee3fd8729a4ea054345563a0a6ba37
stéfany <stelar.dev@gmail.com>
2022-12-28 20:35:55 -0300

    seeds updated

--- src/database/knex/seeds/01_createUsers.js
+++ src/database/knex/seeds/01_createUsers.js
@@ -6,6 +6,7 @@ exports.seed = async function (knex) {
     // Deletes ALL existing entries
     await knex('users').del();
     await knex('users').insert([
-        { id: 1, name: 'Admin', email: 'admin@admin.com', password: '$2y$08$D9CjQeQWd7fsEUnAiLdDeuqB6EBa1mjYR5FjMkoOCM9XgwPPZ..E2', isAdmin: 1 }
+        { id: 1, name: 'Admin', email: 'admin@admin.com', password: '$2b$10$Bagq746yTPH62KETb.tBm.PKJYTi/rAjMNqwuxIa8uh8CZP6eIT5q', isAdmin: 1 },
+        { id: 2, name: 'Jane', email: 'jane@doe.com', password: '$2b$10$Bagq746yTPH62KETb.tBm.PKJYTi/rAjMNqwuxIa8uh8CZP6eIT5q', isAdmin: 0 }
     ]);
 };
--- src/database/knex/seeds/02_createMeals.js
+++ src/database/knex/seeds/02_createMeals.js
@@ -6,23 +6,101 @@ exports.seed = async function (knex) {
     // Deletes ALL existing entries
     await knex('meals').del();
     await knex('meals').insert([
-        { id: 1, name: 'Salada 1', description: 'Teste teste', price: 17.99, picture: '', type: 'Pratos Principais' },
-        { id: 2, name: 'Salada 2', description: 'Teste teste', price: 17.99, picture: '', type: 'Pratos Principais' },
-        { id: 3, name: 'Salada 3', description: 'Teste teste', price: 17.99, picture: '', type: 'Pratos Principais' },
-        { id: 4, name: 'Salada 4', description: 'Teste teste', price: 17.99, picture: '', type: 'Pratos Principais' },
-        { id: 5, name: 'Salada 5', description: 'Teste teste', price: 17.99, picture: '', type: 'Pratos Principais' },
-        { id: 6, name: 'Salada 6', description: 'Teste teste', price: 17.99, picture: '', type: 'Pratos Principais' },
-        { id: 7, name: 'Salada 7', description: 'Teste teste', price: 17.99, picture: '', type: 'Sobremesas' },
-        { id: 8, name: 'Salada 8', description: 'Teste teste', price: 17.99, picture: '', type: 'Sobremesas' },
-        { id: 9, name: 'Salada 9', description: 'Teste teste', price: 17.99, picture: '', type: 'Sobremesas' },
-        { id: 10, name: 'Salada 10', description: 'Teste teste', price: 17.99, picture: '', type: 'Sobremesas' },
-        { id: 11, name: 'Salada 11', description: 'Teste teste', price: 17.99, picture: '', type: 'Sobremesas' },
-        { id: 12, name: 'Salada 12', description: 'Teste teste', price: 17.99, picture: '', type: 'Sobremesas' },
-        { id: 13, name: 'Salada 13', description: 'Teste teste', price: 17.99, picture: '', type: 'Bebidas' },
-        { id: 14, name: 'Salada 14', description: 'Teste teste', price: 17.99, picture: '', type: 'Bebidas' },
-        { id: 15, name: 'Salada 15', description: 'Teste teste', price: 17.99, picture: '', type: 'Bebidas' },
-        { id: 16, name: 'Salada 16', description: 'Teste teste', price: 17.99, picture: '', type: 'Bebidas' },
-        { id: 17, name: 'Salada 17', description: 'Teste teste', price: 17.99, picture: '', type: 'Bebidas' },
-        { id: 18, name: 'Salada 18', description: 'Teste teste', price: 17.99, picture: '', type: 'Bebidas' },
+        {
+            id: 1,
+            name: 'Salada Ravanello',
+            description: 'Rabanetes, folhas verdes e molho agridoce salpicados com gergelim',
+            price: 49.97,
+            picture: '001-ravanello.png',
+            type: 'Pratos Principais'
+        },
+        {
+            id: 2,
+            name: 'Torradas de Parma',
+            description: 'Presunto de parma e rúcula em um pão com fermentação natural.',
+            price: 25.97,
+            picture: '002-parma.png',
+            type: 'Pratos Principais'
+        },
+        {
+            id: 3,
+            name: 'Spaguetti Gambe',
+            description: 'Massa fresca com camarões e pesto.',
+            price: 79.97,
+            picture: '003-gambe.png',
+            type: 'Pratos Principais'
+        },
+        {
+            id: 4,
+            name: 'Salada Molla',
+            description: 'Tomates, coentro, pepino, cebola roxa. Frescos e temperados.',
+            price: 19.97,
+            picture: '004-molla.png',
+            type: 'Pratos Principais'
+        },
+        {
+            id: 5,
+            name: 'Prugna Pie',
+            description: 'Torta de ameixa com massa amanteigada, polvilho em açúcar.',
+            price: 49.97,
+            picture: '005-prugna.png',
+            type: 'Pratos Principais'
+        },
+        {
+            id: 6,
+            name: 'Peachy pastrie',
+            description: 'Delicioso folheado de pêssego com folhas de hortelã.',
+            price: 32.97,
+            picture: '006-peachy.png',
+            type: 'Sobremesas'
+        },
+        {
+            id: 7,
+            name: 'Macarons',
+            description: 'Farinha de amêndoas, manteiga, claras e açúcar.',
+            price: 79.97,
+            picture: '007-macarons.png',
+            type: 'Sobremesas'
+        },
+        {
+            id: 8,
+            name: 'Bolo de damasco',
+            description: 'Damascos frescos em uma massa sem glúten.',
+            price: 19.97,
+            picture: '008-damasco.png',
+            type: 'Sobremesas'
+        },
+        {
+            id: 9,
+            name: 'Suco de maracujá',
+            description: 'Suco de maracujá gelado, cremoso, docinho.',
+            price: 32.97,
+            picture: '009-maracuja.png',
+            type: 'Bebidas'
+        },
+        {
+            id: 10,
+            name: 'Espresso',
+            description: 'Café cremoso feito na temperatura e pressões perfeitas.',
+            price: 9.99,
+            picture: '010-espresso.png',
+            type: 'Bebidas'
+        },
+        {
+            id: 11,
+            name: 'Tè d`autunno',
+            description: 'Chá de anis, canela e limão. Sinta o outono italiano.',
+            price: 17.99,
+            picture: '011-autunno.png',
+            type: 'Bebidas'
+        },
+        {
+            id: 12,
+            name: 'Pomo bourbon ',
+            description: 'Maçã, whisky, canela. On the rocks.',
+            price: 12.99,
+            picture: '012-bourbon.png',
+            type: 'Bebidas'
+        },
     ]);
 };
